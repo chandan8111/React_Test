33f22df78d1f6e90b39cc225c718adf6ad928a9e
Chandan <bestangleon@gmail.com>
2025-11-04 21:11:43 +0530

    import and export component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import User from "./user";
+import User,{Profile} from "./user";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +13,7 @@ function App() {
       <h3>Sum of 5 + 5 : {sum()}</h3>
       <h1>Counter Vol : {count}</h1>
       <User />
+      <Profile />
       <button onClick={() => setCount((count) => count + 1)}>
         Increase Counter
       </button>
--- src/user.jsx
+++ src/user.jsx
@@ -2,4 +2,8 @@ function user() {
     return <h1>User Component</h1>;
 }
 
+export function Profile() {
+    return <h2>Profile Component</h2>;
+}
+
 export default user;
