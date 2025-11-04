6e2e8cc7acaa48eae0c9a1b293559f3714380c86
Chandan <bestangleon@gmail.com>
2025-11-04 20:24:46 +0530

    Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import User from "./user";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +10,12 @@ function App() {
   return (
     <>
       <h1>Code By Me</h1>
-      <h2>Hello React 19</h2>
+      
+      <h1>Counter Vol : {count}</h1>
+      <User />
+      <button onClick={() => setCount((count) => count + 1)}>
+        Increase Counter
+      </button>
     </>
   );
 }
